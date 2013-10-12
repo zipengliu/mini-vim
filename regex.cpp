2ddc6017935e422f8e0410654f074f99b9a57da7
ZqLan <lzq82555906@sina.com>
2013-10-12 23:42:11 +0800

    Program annotation added.

--- regex.cpp
+++ regex.cpp
@@ -1,22 +1,27 @@
+/*************************************************
+文件名称：   regex.cpp
+项目名称：  mini-vim
+创建者：    兰兆千
+创建时间：   2013-10-09
+最后修改时间： 2013-10-12
+功能：     实现正则表达式搜索功能
+与其他文件的依赖关系：
+    头文件regex.h
+**************************************************/
+
+
 #include "regex.h"
 
-struct _RENODE;
-struct _RFANODE;
-struct _RFA;
-struct _DFANODE;
-struct _DFA;
+
+//全局变量区   均用于生成DFA的过程
+queue<_RENODE*> renodes = queue<_RENODE*>();   //构造树状态队列
+stack<_RFA*> rfas = stack<_RFA*>();     //RFA栈
+queue<_RFANODE*> rfanodes = queue<_RFANODE *>();    //RFA状态栈
+set<_DFANODE*> dfanodes = set<_DFANODE*>(); //DFA状态集合
 
 
 
-queue<_RENODE*> renodes = queue<_RENODE*>();
-stack<_RFA*> rfas = stack<_RFA*>();
-queue<_RFANODE*> rfanodes = queue<_RFANODE *>();
-set<_DFANODE*> dfanodes = set<_DFANODE*>();
-
-
-
-
-
+//构造函数
 _RFANODE::_RFANODE(int outc,_RFANODE* out1,_RFANODE* out2)
 {
     id = idcount++;
@@ -25,9 +30,9 @@ _RFANODE::_RFANODE(int outc,_RFANODE* out1,_RFANODE* out2)
     this ->out2 = out2;
     rfanodes.push(this);
 }
+
+
 int RFANODE::idcount = 0;
-
-
 
 
 _RFA::_RFA(RFANODE * pstart)
@@ -36,8 +41,6 @@ _RFA::_RFA(RFANODE * pstart)
     this ->pstart = pstart;
 
 }
-
-
 
 
 _DFANODE::_DFANODE(bool *list)
@@ -66,7 +69,13 @@ _REGEX_RESULT::_REGEX_RESULT(int line ,int start,int len, _REGEX_RESULT* prev , 
 }
 
 
-
+/*************************************************
+函数名称：   gen_tree
+功能：      根据输入的正则表达式，生成后缀表达式
+            （递归调用）
+输入参数：正则表达式字符串指针，长度，目前已经匹配到的位置
+返回值：  匹配结束后匹配到的位置
+*************************************************/
 int gen_tree(const char * word , int len , int pos )
 {
     int natom = 0 , nalt = 0;
@@ -181,7 +190,13 @@ int gen_tree(const char * word , int len , int pos )
 }
 
 
-
+/*************************************************
+函数名称：   rfa_patch
+功能：      将两个rfa顺序连接起来，第一个rfa的
+            终止状态是第二个rfa的开始状态
+输入参数：两个待操作的rfa指针
+返回值：  连接完毕的rfa指针
+*************************************************/
 RFA * rfa_patch(RFA *p1 , RFA *p2)
 {
     for (set<RFANODE *>::iterator it = p1 ->pends.begin(); it != p1 ->pends.end() ; it++)
@@ -195,7 +210,13 @@ RFA * rfa_patch(RFA *p1 , RFA *p2)
     return p2;
 }
 
-
+/*************************************************
+函数名称：   rfa_merge
+功能：      将两个rfa顺序连接起来，头部相接，尾部相接
+        得到一个具有两个分支的rfa
+输入参数：两个待操作的rfa指针
+返回值：  连接完毕的rfa指针
+*************************************************/
 RFA * rfa_merge(RFA *p1 , RFA *p2)
 {
     RFANODE* pnode = new RFANODE(SPLIT,p1 ->pstart,p2 ->pstart);
@@ -218,6 +239,16 @@ RFA * rfa_merge(RFA *p1 , RFA *p2)
     return p;
 }
 
+
+
+/*************************************************
+函数名称：   rfa_oneornone
+功能：      将一个RFA改为能够识别1次或者0次目前该
+        RFA能够识别的语言的RFA
+        对应的是正则表达式中的‘？’
+输入参数：待操作的rfa指针
+返回值：  改造完毕的rfa指针
+*************************************************/
 RFA * rfa_oneornone(RFA *p1)
 {
     RFANODE* pnode = new RFANODE(SPLIT,null,p1 ->pstart);
@@ -226,7 +257,14 @@ RFA * rfa_oneornone(RFA *p1)
     return p1;
 }
 
-
+/*************************************************
+函数名称：   rfa_anytimes
+功能：      将一个RFA改为能够识别 任意次 目前该
+        RFA能够识别的语言 的RFA
+        对应的是正则表达式中的‘*’
+输入参数：待操作的rfa指针
+返回值：  改造完毕的rfa指针
+*************************************************/
 RFA * rfa_anytimes(RFA *p1)
 {
     RFANODE* pnode = new RFANODE(SPLIT,null,p1 ->pstart);
@@ -242,7 +280,14 @@ RFA * rfa_anytimes(RFA *p1)
     return p1;
 }
 
-
+/*************************************************
+函数名称：   rfa_morethanonce
+功能：      将一个RFA改为能够识别 对于一次 目前该
+        RFA能够识别的语言 的RFA
+        对应的是正则表达式中的‘+’
+输入参数：待操作的rfa指针
+返回值：  改造完毕的rfa指针
+*************************************************/
 RFA * rfa_morethanonce(RFA * p1)
 {
     RFANODE* pnode = new RFANODE(SPLIT,null,p1 ->pstart);
@@ -257,28 +302,36 @@ RFA * rfa_morethanonce(RFA * p1)
     return p1;
 }
 
+
+
+/*************************************************
+函数名称：   gen_rfa
+功能：     根据解析树得到的后缀表达式构造RFA
+输入参数：无 （但利用到全局变量renodes）
+返回值：  建立的RFA指针
+*************************************************/
 RFA * gen_rfa()
 {
     RFA * prfa = null;
     RFA * prfa2 = null;
     RFANODE *prfan = null;
     RENODE *prenode = null;
-    while(!renodes.empty())
+    while(!renodes.empty()) //未完成构造，后缀表达式里面还有元素
     {
-        prenode = renodes.front();
+        prenode = renodes.front();  //取出队首元素
         renodes.pop();
-        if (prenode ->type == CHAR)
+        if (prenode ->type == CHAR) //如果是字符，创建一个新的RFA，只接受这一个字符
         {
             prfan = new RFANODE(prenode ->content,null,null);
             prfa = new RFA(prfan);
             prfa ->pends.insert(prfan);
             rfas.push(prfa);
         }
-        else if (prenode ->type == OP)
+        else if (prenode ->type == OP)  //如果是运算符，取出RFA栈中的若干元素进行修改或者合并操作
         {
             switch(prenode ->content)
             {
-            case '.':
+            case '.':   //连接运算符
 				if (rfas.size() < 2)
 					return null;
                 prfa2 = rfas.top();
@@ -288,7 +341,7 @@ RFA * gen_rfa()
                 prfa = rfa_patch(prfa,prfa2);
                 rfas.push(prfa);
                 break;
-            case '|':
+            case '|':   //分支运算符
 				if (rfas.size() < 2)
 					return null;
                 prfa2 = rfas.top();
@@ -298,7 +351,7 @@ RFA * gen_rfa()
                 prfa = rfa_merge(prfa,prfa2);
                 rfas.push(prfa);
                 break;
-            case '?':
+            case '?':   //只重复一次或0次
 				if (rfas.size() < 1)
 					return null;
                 prfa = rfas.top();
@@ -306,7 +359,7 @@ RFA * gen_rfa()
                 prfa = rfa_oneornone(prfa);
                 rfas.push(prfa);
                 break;
-            case '*':
+            case '*':   //重复任意次
 				if (rfas.size() < 1)
 					return null;
                 prfa = rfas.top();
@@ -314,7 +367,7 @@ RFA * gen_rfa()
                 prfa = rfa_anytimes(prfa);
                 rfas.push(prfa);
                 break;
-            case '+':
+            case '+':   //重复一次及以上
 				if (rfas.size() < 1)
 					return null;
                 prfa = rfas.top();
@@ -325,10 +378,18 @@ RFA * gen_rfa()
             }
         }
     }
+
+    //最后应该只剩下一个RFA
 	if (rfas.size() != 1)
 		return null;
+
+
+
 	prfa = rfas.top();
 	rfas.pop();
+
+
+	//将RFA的最后添加一个空状态，作为结束状态
 	RFANODE* pnode = new RFANODE(RFA_END,null,null);
 	for (set<RFANODE *>::iterator it = prfa ->pends.begin(); it != prfa ->pends.end() ; it++)
     {
@@ -340,6 +401,15 @@ RFA * gen_rfa()
     prfa ->pends.insert(pnode);
     return prfa;
 }
+
+
+
+/*************************************************
+函数名称：   rfa_output
+功能：     向控制台输出一个RFA的信息
+输入参数：要输出的RFA
+返回值：  空
+*************************************************/
 void rfa_output(RFA * prfa)
 {
 	cout<< "--------------------RFA info---------------------" << endl;
@@ -372,6 +442,14 @@ void rfa_output(RFA * prfa)
 }
 
 
+
+
+/*************************************************
+函数名称：   gen_eplosure
+功能：    得到一个RFA状态集合的ε闭包
+输入参数：RFA状态集合的引用
+返回值：  指示状态集合中的是否包含某RFA状态的布尔型数组
+*************************************************/
 bool* gen_eplosure(vector<RFANODE*> & rfans)
 {
     bool* list = new bool[RFANODE::idcount + 2];
@@ -397,7 +475,13 @@ bool* gen_eplosure(vector<RFANODE*> & rfans)
 }
 
 
-
+/*************************************************
+函数名称：   gen_shift
+功能：    得到一个RFA状态集合的输入某字符后
+            状态转移得到的状态集合
+输入参数：RFA状态集合的引用，转移字符
+返回值：  状态转移得到的状态集合
+*************************************************/
 vector<RFANODE*> gen_shift(vector<RFANODE*> & rfans,char c)
 {
     vector<RFANODE*> rfans_new = vector<RFANODE*>();
@@ -412,6 +496,14 @@ vector<RFANODE*> gen_shift(vector<RFANODE*> & rfans,char c)
     return rfans_new;
 }
 
+
+
+/*************************************************
+函数名称：   get_next_chars
+功能：    得到一个RFA状态集合可以输入那些字符进入下一个状态集合
+输入参数：RFA状态集合引用
+返回值：  能够进行转移的字符集合
+*************************************************/
 vector<char>  get_next_chars(vector<RFANODE*> & rfans)
 {
     vector<char> cset = vector<char>();
@@ -426,6 +518,13 @@ vector<char>  get_next_chars(vector<RFANODE*> & rfans)
 }
 
 
+
+/*************************************************
+函数名称：   find_dfa
+功能：    根据DFA状态代表的RFA状态集合，寻找DFA状态
+输入参数：指示所代表的RFA状态的bool数组
+返回值：  寻找到的DFA状态，未找到则返回null
+*************************************************/
 DFANODE* find_dfa(const bool *list)
 {
     bool * list2;
@@ -448,6 +547,13 @@ DFANODE* find_dfa(const bool *list)
 }
 
 
+
+/*************************************************
+函数名称：   gen_dfa_node
+功能：    根据DFA状态代表的RFA状态集合，生成一个新的DFA状态
+输入参数：RFA状态集合
+返回值：  建立的DFA状态
+*************************************************/
 DFANODE * gen_dfa_node(vector<RFANODE*> & rfans)
 {
     bool* list = gen_eplosure(rfans);
@@ -468,6 +574,14 @@ DFANODE * gen_dfa_node(vector<RFANODE*> & rfans)
 
 }
 
+
+
+/*************************************************
+函数名称：   dfa_output
+功能：    向控制台输出DFA信息
+输入参数：DFA指针
+返回值：  无
+*************************************************/
 void dfa_output(DFA* pdfa)
 {
 	cout<< "--------------------DFA info---------------------" << endl;
@@ -491,6 +605,13 @@ void dfa_output(DFA* pdfa)
 }
 
 
+
+/*************************************************
+函数名称：   gen_dfa
+功能：    根据一个RFA生成一个DFA
+输入参数：RFA指针
+返回值：  生成的DFA指针
+*************************************************/
 DFA * gen_dfa(RFA * prfa)
 {
     DFA* pdfa = new DFA();
@@ -501,7 +622,12 @@ DFA * gen_dfa(RFA * prfa)
 }
 
 
-
+/*************************************************
+函数名称：   find_match_in_line
+功能：    根据一行的字符，寻找匹配，并将结果插入双向链表
+输入参数：DFA指针，字符串指针，字符串长度len，行号，结果插入位置
+返回值：  找到的匹配个数
+*************************************************/
 int find_match_in_line(DFA* pdfa , char* c , int len , int line , REGEX_RESULT* &r)
 {
 	if (c == null)
@@ -566,6 +692,13 @@ int find_match_in_line(DFA* pdfa , char* c , int len , int line , REGEX_RESULT* 
 	return matchcount;
 }
 
+
+/*************************************************
+函数名称：   regex_match
+功能：    根据DFA和一篇文章，寻找匹配
+输入参数：DFA指针，文章按行存储的双向链表头，结果双向链表头引用
+返回值：  找到的匹配个数
+*************************************************/
 int regex_match(DFA* pdfa , line_t* head , REGEX_RESULT* &r)
 {
 	assert(pdfa != null);
@@ -576,6 +709,7 @@ int regex_match(DFA* pdfa , line_t* head , REGEX_RESULT* &r)
 	int matchcount = 0;
 	while(pline != null)
 	{
+	    //按行搜索，结果相加
 		matchcount += find_match_in_line(pdfa,pline ->content,pline ->len,line,presult);
 		pline = pline ->next;
 		line++;
@@ -583,10 +717,21 @@ int regex_match(DFA* pdfa , line_t* head , REGEX_RESULT* &r)
 	return matchcount;
 }
 
+
+
+/*************************************************
+函数名称：   gen_dfa(char * regex , int len)
+功能：    根据正则表达式，得到DFA
+        将前面所有的过程封装
+输入参数：正则表达式的字符串头指针，长度
+返回值：  生成的DFA
+*************************************************/
 DFA* gen_dfa(char * regex , int len)
 {
+    //先生成构造树得到后缀表达式
 	gen_tree(regex,len,0);
 
+    //然后生成RFA
     RFA * prfa = gen_rfa();
 	if (prfa == null)
     {
@@ -596,6 +741,7 @@ DFA* gen_dfa(char * regex , int len)
     }
     //rfanodes_output(prfa);
 
+    //再生成DFA
     DFA * pdfa = gen_dfa(prfa);
     //dfanodes_output(pdfa);
 
@@ -604,6 +750,15 @@ DFA* gen_dfa(char * regex , int len)
 	return pdfa;
 }
 
+
+
+
+/*************************************************
+函数名称：   regex_release
+功能：    释放所有资源
+输入参数：DFA
+返回值：  无
+*************************************************/
 void regex_release(DFA* pdfa)
 {
 	while(!rfanodes.empty())
@@ -627,9 +782,20 @@ void regex_release(DFA* pdfa)
     }
 	if (pdfa != null)
 		delete pdfa;
+    RFANODE::idcount = 0;
+    DFANODE::idcount = 0;
 }
 
 
+
+
+/*************************************************
+函数名称：   regex_match
+功能：    根据文章，正则表达式，返回匹配结果
+            是对外的接口，之前所有过程的封装
+输入参数：文章双向链表，正则表达式头，正则表达式长度，结果引用
+返回值：  匹配个数 ， -1则是正则表达式不合法
+*************************************************/
 int regex_match(line_t* head ,char * regex , int len , REGEX_RESULT* &r)
 {
     DFA* pdfa = gen_dfa(regex ,len);
--- regex.h
+++ regex.h
@@ -1,3 +1,13 @@
+/*************************************************
+文件名称：   regex.h
+项目名称：  mini-vim
+创建者：    兰兆千
+创建时间：   2013-10-09
+最后修改时间： 2013-10-12
+功能：     实现正则表达式搜索功能
+与其他文件的依赖关系：
+    头文件buffer.h  文章缓存存储结构的定义
+**************************************************/
 #ifndef __MINIVIM_REGEX__
 #define __MINIVIM_REGEX__ 1
 
@@ -18,51 +28,62 @@ enum DFA_NODE_TYPE{DFA_DEFAULT = 0,DFA_END = -1};
 enum DFA_TYPE{DFA_NORMAL = 1,DFA_ABNORMAL = -1};
 
 
-
+//解析树结点
 typedef struct _RENODE
 {
-    NODE_TYPE type;
-    char content;
+    NODE_TYPE type; //类型  字符型和运算符
+    char content; //内容
 }RENODE;
 
+
+//RFA状态
 typedef struct _RFANODE
 {
-    static int idcount;
+    static int idcount;//总共有几个结点
     int id;
-    int outc;
-    _RFANODE* out1;
-    _RFANODE* out2;
+    int outc;//进行到下一个状态所需要的字符，不过也可以是分支、结束
+    _RFANODE* out1;//outc所指示要连接到下一个结点的指针
+    _RFANODE* out2;//分支节点则启用，指向分支
 
     _RFANODE(int outc,_RFANODE* out1,_RFANODE* out2 = null);
 
 }RFANODE;
+
+//RFA
 typedef struct _RFA
 {
-    RFANODE * pstart;
-    set<RFANODE *> pends;
+    RFANODE * pstart; //开始结点
+    set<RFANODE *> pends; //结束结点
     _RFA(RFANODE * pstart);
 }RFA;
 
+
+
+//DFA状态
 typedef struct _DFANODE
 {
     static int idcount;
     int id;
-    _DFANODE* next[CHARSET];
-    bool *list;
-	DFA_NODE_TYPE type;
+    _DFANODE* next[CHARSET];    //状态转移表
+    bool *list; //对应的RFA状态
+	DFA_NODE_TYPE type; //状态类型  是否为结束
     _DFANODE(bool *list);
 
 }DFANODE;
 
+
+//DFA
 typedef struct _DFA
 {
-    DFANODE* pstart;
+    DFANODE* pstart;//开始状态
 }DFA;
 
 
+
+//返回的匹配结果  双向链表
 typedef struct _REGEX_RESULT
 {
-	int line, start , len;
+	int line, start , len;//行号 开始索引 长度
 	struct _REGEX_RESULT *prev , *next;
 	_REGEX_RESULT(int line ,int start,int len, _REGEX_RESULT* prev , _REGEX_RESULT* next);
 }REGEX_RESULT;
